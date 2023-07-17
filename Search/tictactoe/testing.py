58dc18a0becf142b950d19b44f116c9643996d2b
Daniel <46487826+Dmarky9873@users.noreply.github.com>
2023-07-17 00:21:49 -0400

    Moved things around

---
+++ Search/tictactoe/testing.py
@@ -0,0 +1,13 @@
+from tictactoe import *
+
+board = [[O, X, O],
+         [EMPTY, X, EMPTY],
+         [EMPTY, EMPTY, EMPTY]]
+best = float('-inf')
+
+
+""" 
+[[EMPTY, EMPTY, EMPTY],
+[EMPTY, EMPTY, EMPTY],
+[EMPTY, EMPTY, EMPTY]]
+"""
