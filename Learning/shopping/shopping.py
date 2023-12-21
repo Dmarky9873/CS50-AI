4284c10dd8829b796bff569536ff46cd3ef1efb6
Daniel <46487826+Dmarky9873@users.noreply.github.com>
2023-12-21 17:49:54 -0500

    started shopping

--- Learning/shopping/shopping.py
+++ Learning/shopping/shopping.py
@@ -32,69 +32,34 @@ def main():
 
 
 def load_data(filename):
-    """Loads shopping data from a CSV file `filename` and converts it into a list of
-    evidence lists and a list of labels. 
+    """
+    Load shopping data from a CSV file `filename` and convert into a list of
+    evidence lists and a list of labels. Return a tuple (evidence, labels).
 
-    Returns:
-        tuple: Returns a tuple (`evidence`, `labels`).
+    evidence should be a list of lists, where each list contains the
+    following values, in order:
+        - Administrative, an integer
+        - Administrative_Duration, a floating point number
+        - Informational, an integer
+        - Informational_Duration, a floating point number
+        - ProductRelated, an integer
+        - ProductRelated_Duration, a floating point number
+        - BounceRates, a floating point number
+        - ExitRates, a floating point number
+        - PageValues, a floating point number
+        - SpecialDay, a floating point number
+        - Month, an index from 0 (January) to 11 (December)
+        - OperatingSystems, an integer
+        - Browser, an integer
+        - Region, an integer
+        - TrafficType, an integer
+        - VisitorType, an integer 0 (not returning) or 1 (returning)
+        - Weekend, an integer 0 (if false) or 1 (if true)
 
-        `evidence` will be a list of lists, where each list contains the
-        following values, in order:
-        - index: 0 | Administrative, an integer
-        - index: 1 | Administrative_Duration, a floating point number
-        - index: 2 | Informational, an integer
-        - index: 3 | Informational_Duration, a floating point number
-        - index: 4 | ProductRelated, an integer
-        - index: 5 | ProductRelated_Duration, a floating point number
-        - index: 6 | BounceRates, a floating point number
-        - index: 7 | ExitRates, a floating point number
-        - index: 8 | PageValues, a floating point number
-        - index: 9 | SpecialDay, a floating point number
-        - index: 10 | Month, an index from 0 (January) to 11 (December)
-        - index: 11 | OperatingSystems, an integer
-        - index: 12 | Browser, an integer
-        - index: 13 | Region, an integer
-        - index: 14 | TrafficType, an integer
-        - index: 15 | VisitorType, an integer 0 (not returning) or 1 (returning)
-        - index: 16 | Weekend, an integer 0 (if false) or 1 (if true)
-
-    `labels` will be the corresponding list of labels, where each label
+    labels should be the corresponding list of labels, where each label
     is 1 if Revenue is true, and 0 otherwise.
     """
-
-    # Opens the CSV file
-    with open(filename, newline='') as csvfile:
-        reader = csv.reader(csvfile)
-
-        evidences = []
-        labels = []
-
-        # Adds everything to the coresponding lists and cleans the data
-        for line in list(reader)[1:]:
-            evidence = []
-            evidence.append(int(line[0]))
-            evidence.append(float(line[1]))
-            evidence.append(int(line[2]))
-            evidence.append(float(line[3]))
-            evidence.append(int(line[4]))
-            evidence.append(float(line[5]))
-            evidence.append(float(line[6]))
-            evidence.append(float(line[7]))
-            evidence.append(float(line[8]))
-            evidence.append(float(line[9]))
-            evidence.append(getMonthInt(line[10]))
-            evidence.append(int(line[11]))
-            evidence.append(int(line[12]))
-            evidence.append(int(line[13]))
-            evidence.append(int(line[14]))
-            evidence.append(getVisitorTypeInt(line[15]))
-            evidence.append(getBooleanInt(line[16]))
-
-            evidences.append(evidence)
-
-            labels.append(getBooleanInt(line[17]))
-
-    return (evidences, labels)
+    raise NotImplementedError
 
 
 def train_model(evidence, labels):
@@ -102,10 +67,7 @@ def train_model(evidence, labels):
     Given a list of evidence lists and a list of labels, return a
     fitted k-nearest neighbor model (k=1) trained on the data.
     """
-    model = KNeighborsClassifier(n_neighbors=1)
-    model.fit(evidence, labels)
-
-    return model
+    raise NotImplementedError
 
 
 def evaluate(labels, predictions):
@@ -123,62 +85,7 @@ def evaluate(labels, predictions):
     representing the "true negative rate": the proportion of
     actual negative labels that were accurately identified.
     """
-
-    pred_trues = 0
-    pred_falses = 0
-    real_trues = 0
-    real_falses = 0
-
-    for label, prediction in zip(labels, predictions):
-        if label == 1:
-            real_trues += 1
-            if label == prediction:
-                pred_trues += 1
-        elif label == 0:
-            real_falses += 1
-            if label == prediction:
-                pred_falses += 1
-
-    return (pred_trues/real_trues, pred_falses/real_falses)
-
-
-def getMonthInt(month: str):
-    if month == 'Jan':
-        return 0
-    elif month == 'Feb':
-        return 1
-    elif month == 'Mar':
-        return 2
-    elif month == 'Apr':
-        return 3
-    elif month == 'May':
-        return 4
-    elif month == 'June':
-        return 5
-    elif month == 'Jul':
-        return 6
-    elif month == 'Aug':
-        return 7
-    elif month == 'Sep':
-        return 8
-    elif month == 'Oct':
-        return 9
-    elif month == 'Nov':
-        return 10
-    elif month == 'Dec':
-        return 11
-
-
-def getVisitorTypeInt(visitorType: str):
-    if visitorType == 'Returning_Visitor':
-        return 1
-    return 0
-
-
-def getBooleanInt(boolean: str):
-    if boolean == 'TRUE':
-        return 1
-    return 0
+    raise NotImplementedError
 
 
 if __name__ == "__main__":
