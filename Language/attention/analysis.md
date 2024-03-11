1bc9b8cf58e0723f3e8cb0e98e578c89a1f98ba6
Daniel Markusson <46487826+Dmarky9873@users.noreply.github.com>
2024-03-10 21:21:51 -0400

    finished attention

--- Language/attention/analysis.md
+++ Language/attention/analysis.md
@@ -1,18 +1,19 @@
 # Analysis
 
-## Layer TODO, Head TODO
+## Layer 1, Head 4
 
-TODO
-
-Example Sentences:
-- TODO
-- TODO
-
-## Layer TODO, Head TODO
-
-TODO
+The model found strong relationships between word pairs like "turned" with "down" and "passed" with "through." It seems like this head's role is to identify relationships between two-word pairs.
 
 Example Sentences:
-- TODO
-- TODO
 
+- We turned down a [MASK] lane and passed through a small clearing.
+- Daniel found a new pair of [MASK] that he likes to wear.
+
+## Layer 4, Head 12
+
+This head seems responsible for main words and finding relationships between them, cutting out the words that are less necissary to carry the main idea of the sentence.
+
+Example Sentences:
+
+- I enjoyed the [MASK] today, but it went a little too long.
+- Don't go in [MASK]!
--- Language/attention/mask.py
+++ Language/attention/mask.py
@@ -4,6 +4,8 @@ import tensorflow as tf
 from PIL import Image, ImageDraw, ImageFont
 from transformers import AutoTokenizer, TFBertForMaskedLM
 
+import math
+
 # Pre-trained masked language model
 MODEL = "bert-base-uncased"
 
@@ -45,9 +47,11 @@ def get_mask_token_index(mask_token_id, inputs):
     Return the index of the token with the specified `mask_token_id`, or
     `None` if not present in the `inputs`.
     """
-    # TODO: Implement this function
-    raise NotImplementedError
 
+    for index, token in enumerate(inputs['input_ids'][0]):
+        if token == mask_token_id:
+            return index
+    return None
 
 
 def get_color_for_attention_score(attention_score):
@@ -55,9 +59,8 @@ def get_color_for_attention_score(attention_score):
     Return a tuple of three integers representing a shade of gray for the
     given `attention_score`. Each value should be in the range [0, 255].
     """
-    # TODO: Implement this function
-    raise NotImplementedError
-
+    n = round(255*float(attention_score))
+    return (n, n, n)
 
 
 def visualize_attentions(tokens, attentions):
@@ -71,12 +74,16 @@ def visualize_attentions(tokens, attentions):
     (starting count from 1).
     """
     # TODO: Update this function to produce diagrams for all layers and heads.
-    generate_diagram(
-        1,
-        1,
-        tokens,
-        attentions[0][0][0]
-    )
+    # generate_diagram(
+    #     1,
+    #     1,
+    #     tokens,
+    #     attentions[0][0][0]
+    # )
+
+    for i, layer in enumerate(attentions):
+        for j, index in enumerate(layer[0]):
+            generate_diagram(i + 1, j + 1, tokens, index)
 
 
 def generate_diagram(layer_number, head_number, tokens, attention_weights):
